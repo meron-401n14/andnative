cff25ce922e8f7f21f00f62356d1833415f14d93
meron <malemu17@yahoo.com>
2020-02-17 13:35:25 -0800

    toworkonanothercomputer

---
+++ README.md
@@ -0,0 +1,41 @@
+### LAB - React Native ToDo Application
+
+
+
+##### Author: Meron
+
+##### Links and Resources
+* [submission PR](https://github.com/malemu17/newlab-30)
+* [travis](https://travis-ci.com/malemu17/newlab-30/builds/148021511)
+* [Heroku](https://to-donew.herokuapp.com/)
+* [codesandbox.io](https://codesandbox.io/s/github/malemu17/newlab-30)
+
+
+#### Documentation
+* [jsdoc](comments only)
+
+
+
+### Modules
+* Login/Logout
+* Task.js/TaskAdd.js/TaskList.js/Taskedit.js
+* store/authReducer.js/index.js/taskReducer.js
+* stylesheet.js/AuthoViewr.js/App.js/utils.js
+
+
+### Setup
+#### `.env` requirements
+* Plat form Android 
+* backend deployed on Heroku 
+
+#### Running the app
+* `npm start`
+* Endpoint: `/all-tasks/mark-undone/mark-done/update-task/delete-task`
+
+  
+#### Tests
+* No test
+
+
+#### UML
+- ![UML](lab3031.jpg)
---
+++ TaskAdd.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function TaskAdd() {
+    return (
+        <View>
+            <Text>In Task Add</Text>
+        </View>
+    );
+}
+
+export default TaskAdd;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { dateFix } from './util';
+// import { View, Text, Switch, TextInput, Button } from 'react-native';
+
+
+// function TaskAdd(props) {
+//     let [name, setName] = useState('');
+//     let [description, setDescription] = useState('');
+//     let [priority, setPriority] = useState('');
+//     let [date, setDate] = useState('');
+//     let [isComplete, setIsComplete] = useState(false);
+
+
+    
+//     let h1 = {
+//         fontSize: 60,
+//         textAlign: 'center',
+//     };
+
+//     let label = {
+//         fontSize: 30,
+//         color: '#555',
+//     };
+
+//     return (
+//         <View>
+//             <Input
+//                 id='add-task-form'
+//                 onSubmit={e => {
+//                     e.preventDefault();
+//                     setName('');
+//                     setDescription('');
+//                     setPriority('');
+//                     setDate('');
+//                     setIsComplete(false);
+//                 }}
+//             >
+//                 <Text>Add Task</Text>
+//                 <label>
+//                     Name:{' '}
+//                     <TextInput
+//                         type='text'
+//                         value={name}
+//                         onChange={e => {
+//                             setName(e.target.value);
+//                         }}
+//                     />
+//                 </label>
+
+//                 <label>
+//                     Description:
+//                     <TextInput
+//                         type='text'
+//                         value={description}
+//                         onChange={e => {
+//                             setDescription(e.target.value);
+//                         }}
+//                     />
+//                 </label>
+
+//                 <label>
+//                     Priority:
+//                     <TextInput
+//                         type='number'
+//                         value={priority}
+//                         onChange={e => {
+//                             setPriority(e.target.value);
+//                         }}
+//                     />
+//                 </label>
+
+//                 <label>
+//                     Date:
+//                     <TextInput
+//                         type='date'
+//                         value={date}
+//                         onChange={e => {
+//                             setDate(e.target.value);
+//                         }}
+//                     />
+//                 </label>
+
+//                 <label>
+//                     Is Complete?
+//                     <TextInput
+//                         type='checkbox'
+//                         checked={isComplete}
+//                         onChange={e => {
+//                             setIsComplete(e.target.value);
+//                         }}
+//                     />
+//                 </label>
+
+//                 <button
+//                     onPress={() => {
+//                         let data = {
+//                             name,
+//                             description,
+//                             priority,
+//                             isComplete,
+//                             date,
+//                         };
+//                         props.add(data);
+//                     }}
+//                 >
+//                     Save Changes
+//                 </button>
+//                 <button>Clear</button>
+//             </Input>
+//         </View>
+//     );
+// }
+
+// export default TaskAdd;
